f665cb74dfc3ccf701c6cefa064097a0b95108ad
mike mora <mmoramb@gmail.com>
2020-02-13 22:48:13 -0600

    fisrt commit

---
+++ springboottest/src/main/java/com/example/springboottest/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.example.springboottest;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+ 
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByCode(String code);
+}
