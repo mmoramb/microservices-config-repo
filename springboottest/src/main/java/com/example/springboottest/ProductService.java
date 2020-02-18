bfc6a6667dc6362c16b35bc258eb6aa1484fd770
Miguel Mora Bautista <miguel.mora@neoris.com>
2020-02-18 11:33:53 -0600

    changes existing projects

--- springboottest/src/main/java/com/example/springboottest/ProductService.java
+++ springboottest/src/main/java/com/example/springboottest/ProductService.java
@@ -2,8 +2,12 @@ package com.example.springboottest;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+
 import java.util.List;
 import java.util.Optional;
  
@@ -12,17 +16,36 @@ import java.util.Optional;
 @Slf4j
 public class ProductService {
     private final ProductRepository productRepository;
- 
+    private final RestTemplate restTemplate;
+    
     @Autowired
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, RestTemplate restTemplate) {
         this.productRepository = productRepository;
+        this.restTemplate = restTemplate;
     }
  
+    
     public List<Product> findAllProducts() {
         return productRepository.findAll();
     }
  
     public Optional<Product> findProductByCode(String code) {
-        return productRepository.findByCode(code);
+    	Optional<Product> productOptional = productRepository.findByCode(code);
+    	if (productOptional.isPresent()) {
+			ResponseEntity<ProductInventoryResponse> itemEntityRes = restTemplate.getForEntity(
+					"http://inventory-service/api/inventory/{code}", ProductInventoryResponse.class,
+					code);
+			
+			if (itemEntityRes.getStatusCode() == HttpStatus.OK) {
+				Integer quantity = itemEntityRes.getBody().getAvailableQuantity();
+                productOptional.get().setInStock(quantity > 0);
+			} else {
+				System.out.println("Unable to get inventory level for product_code");
+			}
+		}
+    	
+    	return productOptional;
     }
+    
+    
 }
--- springboottest/src/main/java/com/example/springboottest/SpringboottestApplication.java
+++ springboottest/src/main/java/com/example/springboottest/SpringboottestApplication.java
@@ -2,10 +2,19 @@ package com.example.springboottest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringboottestApplication {
-
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringboottestApplication.class, args);
 	}
