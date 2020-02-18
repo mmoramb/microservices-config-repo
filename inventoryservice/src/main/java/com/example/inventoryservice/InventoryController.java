432b480302843c9f85c5bb852bf74ae306d65cd7
Miguel Mora Bautista <miguel.mora@neoris.com>
2020-02-18 11:32:06 -0600

    new projects

---
+++ inventoryservice/src/main/java/com/example/inventoryservice/InventoryController.java
@@ -0,0 +1,35 @@
+package com.example.inventoryservice;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class InventoryController {
+	private final InventoryItemRepository inventoryItemRepository;
+	
+	@Autowired
+	public InventoryController(InventoryItemRepository itemRepository) {
+		inventoryItemRepository = itemRepository;
+	}
+	
+	@GetMapping("/api/inventory/{productCode}")
+	public ResponseEntity<InventoryItem> findInventoryByProductCode(@PathVariable("productCode") String productCode){
+		Optional<InventoryItem> inventoryItem = inventoryItemRepository.findByProductCode(productCode);
+		
+		if (inventoryItem.isPresent()) {
+			return new ResponseEntity(inventoryItem, HttpStatus.OK);
+		} else {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+	}
+}
