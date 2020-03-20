9fdd8a7c0ab59f8db78ee51570781262d27f817c
mike mora <mmoramb1@gmail.com>
2020-03-19 23:47:52 -0600

    spring cloud gateway

---
+++ second-service/src/main/java/com/javainuse/SecondController.java
@@ -0,0 +1,16 @@
+package com.javainuse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consumer")
+public class SecondController {
+	@GetMapping("/message")
+	public String test(@RequestHeader("second-request") String header) {
+		System.out.println(header);
+		return "Hello JavaInUse Called in Second Service";
+	}
+}
